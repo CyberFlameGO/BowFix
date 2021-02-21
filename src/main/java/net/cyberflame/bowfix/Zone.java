e205bbc5ee28eab9cb028ae88eb58a1dc22d3979
CyberFlame <cyberflameu@gmail.com>
2021-02-21 16:37:55 +1300

    Create Zone.java

---
+++ src/main/java/net/cyberflame/bowfix/Zone.java
@@ -0,0 +1,12 @@
+package net.cyberflame.bowfix;
+
+import org.bukkit.Location;
+
+public class Zone {
+  public static boolean contains(Location loc, int x1, int x2, int z1, int z2) {
+    ZoneVector curr = new ZoneVector(loc.getBlockX(), loc.getBlockZ());
+    ZoneVector min = new ZoneVector(Math.min(x1, x2), Math.min(z1, z2));
+    ZoneVector max = new ZoneVector(Math.max(x1, x2), Math.max(z1, z2));
+    return curr.isInAABB(min, max);
+  }
+}
