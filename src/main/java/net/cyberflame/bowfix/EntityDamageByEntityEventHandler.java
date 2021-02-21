36453614055514020be814d4e73d7a3b5f4b6aaf
CyberFlame <cyberflameu@gmail.com>
2021-02-21 16:49:07 +1300

    Update EntityDamageByEntityEventHandler.java

--- src/main/java/net/cyberflame/bowfix/EntityDamageByEntityEventHandler.java
+++ src/main/java/net/cyberflame/bowfix/EntityDamageByEntityEventHandler.java
@@ -27,25 +27,30 @@ public class EntityDamageByEntityEventHandler implements Listener {
       Projectile projectile = (Projectile)e.getDamager();
       if (projectile.getType() == EntityType.ARROW && projectile.getShooter() instanceof Player && ((Player)projectile.getShooter()).getUniqueId().equals(p.getUniqueId())) {
         ItemStack item = p.getItemInHand();
-        if (this.bowFix.getConfig().getBoolean("settings.punch-only", false) && !item.containsEnchantment(Enchantment.ARROW_KNOCKBACK))
-          return; 
+        if (this.bowFix.getConfig().getBoolean("settings.punch-only", false) && !item.containsEnchantment(Enchantment.ARROW_KNOCKBACK)) {
+          return;
+        }
         if (this.bowFix.getConfig().getBoolean("settings.punch-only", true) && item.containsEnchantment(Enchantment.ARROW_KNOCKBACK)) {
           int enchLevel = item.getEnchantmentLevel(Enchantment.ARROW_KNOCKBACK);
-          if (enchLevel != 2)
-            return; 
+          if (enchLevel != 2) {
+            return;
+          }
         } 
+        
         if (e.getEntity().getWorld().getName().startsWith("PlayerWarp") && 
           Zone.contains(e.getEntity().getLocation(), 21, 27, -6, 0)) {
           e.setCancelled(true);
           sendMessage((CommandSender)e.getEntity(), "&cYou cannot use this in a safezone.");
+          
           return;
         } 
+        
         e.setDamage(1.0D);
         this.bowFix.getServer().getScheduler().runTaskLater((Plugin)this.bowFix, () -> {
               Vector velocity = paramPlayer.getEyeLocation().getDirection().multiply(this.bowFix.getConfig().getDouble("settings.velocity-multiplier", 2.5D));
               velocity.setY(0.33D);
               paramPlayer.setVelocity(velocity);
-            });
+            }0L);
       } 
     } 
   }
